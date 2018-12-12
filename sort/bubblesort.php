09897b2bfc0d11f1e8cd2f447e0e82f6bb36a753
caoxiangnewstart <xiang.cao@blockin.com>
2018-12-12 17:10:45 +0800

    冒泡排序

---
+++ sort/bubblesort.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: caoxiang
+ * Date: 2018/12/12
+ * Time: 下午3:50
+ */
+/**
+ * 1 冒泡排序：将每个数字a和剩余其他数字b比较,(升序)如果a>b,那么交换a和b的位置
+ * 2 轮次:n = count($arr),(1+...+n-1)轮
+ * @param $arr
+ */
+function bubblesort($arr){
+    $arr_length = count($arr);
+    //轮数
+    for ($i=1;$i<$arr_length;$i++){
+        //美轮比较的次数
+        for($j=0;$j<$arr_length-$i;$j++){
+            if($arr[$j]>$arr[$j+1]){
+                $temp     = $arr[$j];
+                $arr[$j]=$arr[$j+1];
+                $arr[$j+1]  = $temp;
+            }
+        }
+    }
+    return $arr;
+}
+$result = bubblesort([2,1,3,9,5,6]);
+print_r($result);

f7b1955fc48021ff989ec1ee75905c621e168437
philipp <philipp@philippdeMBP.lan>
2018-12-25 22:35:27 +0800

    大堆排序法

---
+++ sort/HeapSort.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: philipp
+ * Date: 2018-12-25
+ * Time: 19:42
+ */
+
+/**
+ * Class HeapSort
+ * 大根堆特点
+ * 子节点的数据不大于父节点，那么最上面的节点数据一定是该数组中最大的
+ * 主要是利用大堆的特点，每一次构建好大堆之后就可以进行最大数据的抽离(首位)，然后递归处理
+ *
+ */
+class HeapSort{
+    protected $data;
+    protected $count;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+        $this->count = count($data);
+    }
+
+    public function run(){
+        $this->createHeap();
+        while ($this->count > 0){
+            $this->swap($this->data[0],$this->data[--$this->count]);
+            $this->buildHeap($this->data,0,$this->count);
+        }
+        return $this->data;
+    }
+
+    public function createHeap(){
+        //第一次初始化大堆
+        $i = floor($this->count / 2) + 1;
+        while($i--){
+            $this->buildHeap($this->data,$i,$this->count);
+        }
+    }
+
+    public function buildHeap(array &$data,$i,$count){
+        if(false===$i<$count){
+            return ;
+        }
+        //most important
+        $left   =   ($right= 2*$i + 1) + 1;
+        $max    =   $i;
+        //左节点大于父节点，记录左节点的位置
+        if($left < $count && $data[$left] > $data[$i]){
+            $max    =   $left;
+        }
+        //右节点大于父节点，记录右节点的位置
+        if($right < $count && $data[$right] > $data[$max]){
+            $max    =   $right;
+        }
+
+        if($max !== $i && $max < $count){
+            //交换基准节点和最大节点的位置
+            $this->swap($data[$i],$data[$max]);
+            //以最大节点的位置节点为基准节点，递归堆得创建
+            $this->buildHeap($data,$max,$count);
+        }
+    }
+
+    public function swap(&$left,&$right){
+        list($left,$right) = [$right,$left];
+    }
+}
+
+$array  = array (4, 21, 41);
+$result = (new HeapSort($array))->run();
+var_dump($result);

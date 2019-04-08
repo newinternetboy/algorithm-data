c12d6890e646dc81799d2aaf7172e686f5ec4148
caoxiang <961937794@qq.com>
2019-04-08 15:16:30 +0800

    二叉搜索树-搜索不到-插入生成新的树

--- Tree/Binary_Sort_Tree.php
+++ Tree/Binary_Sort_Tree.php
@@ -7,6 +7,7 @@
  */
 
 //首先根节点值得判断，搜索值小于根节点，搜索右子树，反之搜索左子树
+//二叉排序树，在搜索关键字返回false的最后一个节点，插入搜索关键字作为新叶子节点，构成新的二叉排序树
 
 class Binary_Sort_Tree{
     public static $search_path=[];
@@ -28,29 +29,39 @@ class Binary_Sort_Tree{
         $result = $this->binary_search_item($this->binary_tree,$this->i,$this->search);
         return $result;
     }
-    function binary_search_item($binary_tree,$i,$search){
-        if(empty($binary_tree)){
+    function binary_search_item(&$binary_tree,$i,$search){
+        //记录每次节点比较的值
+        if(isset($binary_tree[$i])){
+            array_push(self::$search_path,$i);
+        }
+        if(!isset($binary_tree[$i])){
             return false;
-        }else{
-            //记录每次节点比较的值
-            if(isset($binary_tree[$i])){
-                array_push(self::$search_path,$i);
-            }
-            if(!isset($binary_tree[$i])){
-                return false;
-            }else if($binary_tree[$i] == $search){
-                return $i;
-            }else if($search > $binary_tree[$i]){
-                $right = 2 * ($i + 1) ;
-                $result = $this->binary_search_item($binary_tree,$right,$search);
-                return $result;
-            }else if($search < $binary_tree[$i]){
-                $left = 2 * ($i + 1)-1;
-                $result = $this->binary_search_item($binary_tree,$left,$search);
-                return $result;
+        }else if($binary_tree[$i] == $search){
+            return $i;
+        }else if($search > $binary_tree[$i]){
+            $right = 2 * ($i + 1) ;
+            //如果没有左结点直接插入,返回true
+            if(!isset($binary_tree[$right])){
+                $binary_tree[$right] = $search;
+                array_push(self::$search_path,$right);
+                $result = true;
             }else{
-                return false;
+                $result = $this->binary_search_item($binary_tree,$right,$search);
             }
+            return $result;
+        }else if($search < $binary_tree[$i]){
+            $left = 2 * ($i + 1)-1;
+            //如果没有右结点直接插入,返回true
+            if(!isset($binary_tree[$left])){
+                $binary_tree[$left] = $search;
+                array_push(self::$search_path,$left);
+                $result = true;
+            }else{
+                $result = $this->binary_search_item($binary_tree,$left,$search);
+            }
+            return $result;
+        }else{
+            return false;
         }
     }
 
@@ -60,6 +71,10 @@ class Binary_Sort_Tree{
             array_push($item_path,$this->binary_tree[$v]);
         }
         return implode('->',$item_path);
+    }
+
+    public function delNode(){
+        //
     }
 }
 
@@ -81,7 +96,7 @@ $binary_tree = [
     13 => 61,
     14 => null
 ];
-$binary_sort_tree = new Binary_Sort_Tree($binary_tree,0,61);
+$binary_sort_tree = new Binary_Sort_Tree($binary_tree,0,62);
 $index = $binary_sort_tree -> run();
 var_dump($index);
 $path = $binary_sort_tree ->getSearchPath();
